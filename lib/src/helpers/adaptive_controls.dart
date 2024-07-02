963191cacde07ba887e9c5adf4103bf6f5d335b0
Ashish Walia <ashish4walia@gmail.com>
2024-07-02 18:32:12 +0530

    use material controls everywhere

--- lib/src/helpers/adaptive_controls.dart
+++ lib/src/helpers/adaptive_controls.dart
@@ -8,23 +8,6 @@ class AdaptiveControls extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    switch (Theme.of(context).platform) {
-      case TargetPlatform.android:
-      case TargetPlatform.fuchsia:
-        return const MaterialControls();
-
-      case TargetPlatform.macOS:
-      case TargetPlatform.windows:
-      case TargetPlatform.linux:
-        return const MaterialDesktopControls();
-
-      case TargetPlatform.iOS:
-        return const CupertinoControls(
-          backgroundColor: Color.fromRGBO(41, 41, 41, 0.7),
-          iconColor: Color.fromARGB(255, 200, 200, 200),
-        );
-      default:
-        return const MaterialControls();
-    }
+     return const MaterialControls();
   }
 }
